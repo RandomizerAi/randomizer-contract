843eb1d6c908a6403f7b5cff1a299dc5636f64f0
Dean <hello@dean.press>
2022-08-15 15:09:14 +0200

    remove fixed points for ethereum gas handler

--- gas-handler-contracts/Ethereum.sol
+++ gas-handler-contracts/Ethereum.sol
@@ -12,7 +12,7 @@ contract GasHandler {
 
     function _getGasPrice() internal view returns (uint256) {
         uint256 maxFee = block.basefee +
-            (((block.basefee * 1 ether) / 4) / 1 ether);
+            (block.basefee / 4);
         uint256 gasPrice = tx.gasprice < maxFee ? tx.gasprice : maxFee;
         return gasPrice;
     }
